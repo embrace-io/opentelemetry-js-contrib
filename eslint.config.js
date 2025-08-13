db3dce907dba639a8ed01e8fc5713c0afd82566b
overbalance <overbalance@users.noreply.github.com>
2025-08-13 03:13:39 -0500

    fix header

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,42 @@ const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const globals = require('globals');
 const nodePlugin = require('eslint-plugin-n');
+const licenseHeaderPlugin = require('eslint-plugin-license-header');
+
+// Common license header configuration
+const licenseHeader = {
+  'license-header/header': [
+    'error',
+    [
+      '/*',
+      ' * Copyright The OpenTelemetry Authors',
+      ' *',
+      ' * Licensed under the Apache License, Version 2.0 (the "License");',
+      ' * you may not use this file except in compliance with the License.',
+      ' * You may obtain a copy of the License at',
+      ' *',
+      ' *      https://www.apache.org/licenses/LICENSE-2.0',
+      ' *',
+      ' * Unless required by applicable law or agreed to in writing, software',
+      ' * distributed under the License is distributed on an "AS IS" BASIS,',
+      ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
+      ' * See the License for the specific language governing permissions and',
+      ' * limitations under the License.',
+      ' */',
+    ],
+  ],
+};
+
+// Common base rules for all files
+const baseRules = {
+  ...js.configs.recommended.rules,
+  quotes: ['error', 'single', { avoidEscape: true }],
+  eqeqeq: ['error', 'smart'],
+  'prefer-rest-params': 'off',
+  'no-shadow': 'off',
+  'node/no-deprecated-api': ['warn'],
+  ...licenseHeader,
+};
 
 /** @type {Array<import('eslint').Linter.FlatConfig>} */
 const config = [
@@ -21,12 +57,12 @@ const config = [
     files: ['**/*.{js,ts,mjs}'],
     plugins: {
       node: nodePlugin,
+      'license-header': licenseHeaderPlugin,
     },
     languageOptions: {
       ecmaVersion: 2022,
-      // sourceType: 'module',
       parserOptions: {
-        project: true,
+        project: null,
       },
       globals: {
         ...globals.node,
@@ -37,14 +73,7 @@ const config = [
         tryExtensions: ['.js', '.json', '.node', '.ts'],
       },
     },
-    rules: {
-      ...js.configs.recommended.rules,
-      quotes: ['error', 'single', { avoidEscape: true }],
-      eqeqeq: ['error', 'smart'],
-      'prefer-rest-params': 'off',
-      'no-shadow': 'off',
-      'node/no-extraneous-require': 'error',
-    },
+    rules: baseRules,
   },
   // TypeScript-specific config
   {
@@ -52,17 +81,18 @@ const config = [
     plugins: {
       '@typescript-eslint': tseslint.plugin,
       node: nodePlugin,
+      'license-header': licenseHeaderPlugin,
     },
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        project: true,
+        project: null,
       },
     },
     rules: {
+      ...baseRules,
       'no-unused-vars': 'off',
       'no-undef': 'off',
-      ...tseslint.configs.strictTypeChecked.rules,
       '@typescript-eslint/no-this-alias': 'off',
       '@typescript-eslint/no-shadow': ['warn'],
       '@typescript-eslint/no-var-requires': 'off',
@@ -71,6 +101,7 @@ const config = [
         'error',
         { ignoreProperties: true },
       ],
+      '@typescript-eslint/no-unsafe-function-type': 'off',
       '@typescript-eslint/naming-convention': [
         'error',
         {
@@ -84,15 +115,29 @@ const config = [
   },
   // Test files config
   {
-    files: ['**/test/**/*.{js,ts}'],
+    files: ['**/test/**/*.ts'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      node: nodePlugin,
+      'license-header': licenseHeaderPlugin,
+    },
     languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: null,
+      },
       globals: {
         ...globals.mocha,
         ...globals.node,
       },
     },
     rules: {
+      ...baseRules,
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
       'no-empty': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
       '@typescript-eslint/no-empty-function': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
@@ -101,6 +146,13 @@ const config = [
       '@typescript-eslint/no-floating-promises': 'off',
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+  // ES modules config
+  {
+    files: ['**/*.mjs'],
+    languageOptions: {
+      sourceType: 'module',
     },
   },
   // Browser environment config
